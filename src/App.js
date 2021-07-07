b208e32b5f621353a6074e4d4e71d9f056723294
AJ <ancheetah@users.noreply.github.com>
2021-07-06 23:49:39 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [data, updateData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+
+  useEffect( () => {
+    setLoading(true);
+
+    fetch("https://geo.ipify.org/api/v1?apiKey=at_p0qchqpbAVlxMUkOgbHRyaKERKFCe&ipAddress=8.8.8.8")
+      .then(response => {
+        if (response.ok) {
+          // console.log(response.json());
+          return response.json();
+        } else {
+          throw response;
+        }
+      })
+      .then( data => {
+        updateData(data);
+      })
+      .finally(setLoading(false));
+
+  }, [])
+
+  if (loading) {
+    return "Loading..."
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        {
+          data
+          ?
+          JSON.stringify(data)
+          :
+          ""
+        }
+      </p>
     </div>
   );
 }
