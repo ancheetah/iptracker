c693ab77d086526cf8138e5a0d1c8855860c5c44
AJ <ancheetah@users.noreply.github.com>
2021-07-07 04:43:18 -0400

    header component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Container, TextField, InputAdornment, IconButton, Icon} from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
 const styles = {
     color: "white",
@@ -10,6 +12,22 @@ const Header = (props) => {
     return (
         <header style={styles}>
             <h2>IP Address Tracker</h2>
+
+            <Container maxWidth="sm">
+                <form action="" noValidate>
+                    <TextField 
+                        fullWidth
+                        variant="filled"
+                        label="Search for any IP address or domain"
+                        InputProps={{
+                            endAdornment: 
+                                <InputAdornment position="end">
+                                    <IconButton color="primary"><SearchIcon/></IconButton>
+                                </InputAdornment>
+                            }}
+                    />
+                </form>
+            </Container>
         </header>
     );
 }
